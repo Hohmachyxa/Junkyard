993703603c7caece33f87cbac48785c26d8b40d1
student <student@spbacu.ru>
2026-01-24 17:51:26 +0300

    Initial commit

---
+++ src/main/java/org/example/Task.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Task {
+    private int id;
+    public String name;
+    public String description;
+
+    public Task(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+}
